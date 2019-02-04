ebeb7d4d48d02a0a6e10277a34f6966e6eab5d0a
HugoIP <bairon.myh@gmail.com>
2019-02-03 23:43:48 -0600

    First commit

--- controllers/clients_controller.php
+++ controllers/clients_controller.php
@@ -3,5 +3,5 @@ require_once("models/clients_model.php");
 $client=new clients_model();
 $data=$client->get_clients();
 //call view
-require_once("views/clients_views.php");
+require_once("views/clients_view.php");
  ?>
