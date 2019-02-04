07e0a615adbac2a5c2564457296606a5b2497967
HugoIP <bairon.myh@gmail.com>
2019-02-03 23:36:56 -0600

    First commit

--- db/db.php
+++ db/db.php
@@ -1,6 +1,6 @@
 <?php 
 class Connect{
-	public function connect(){
+	public function connection(){
 		$connecting=new mysqli("localhost","hugoip","MONICA","vNovaInternet");
 		$connecting->query("SET NAMES 'utf8'");
 		return $connecting;
