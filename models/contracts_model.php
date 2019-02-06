bf251c44997ee41c5bf4bb38cd281b200251da3d
HugoIP <bairon.myh@gmail.com>
2019-02-06 04:08:46 -0600

    Create contracts controller add is_object validation num_rows

--- models/contracts_model.php
+++ models/contracts_model.php
@@ -24,7 +24,7 @@ class contracts_model{
 	    {
 	     //... 
 	    }
-	    else if (is_object($$query))
+	    else if (is_object($query))
 	    {
 	        $total_num_rows = $query->num_rows;
 	    }
